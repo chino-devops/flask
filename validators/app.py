64ddbc443cbc2f485e02b6f75c22b9976f0cf441
chino-devops <danielnwankwo95@gmail.com>
2021-06-17 01:04:52 +0000

    validators

---
+++ validators/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField # We will only use StringField and SubmitField in our simple form.
+from wtforms.validators import DataRequired, Length, ValidationError
+
+app = Flask(__name__)
+app.config['SECRET_KEY']='SOME_KEY' #Configure a secret key for CSRF protection.
+
+class UserCheck:
+    def __init__(self, banned, message=None):
+        self.banned = banned
+        if not message:
+            message = 'Please choose another username'
+        self.message = message
+
+    def __call__(self, form, field):
+        if field.data.lower() in (word.lower() for word in self.banned):
+            raise ValidationError(self.message)
+
+class myForm(FlaskForm):
+    username = StringField('Username', validators=[
+        DataRequired(),
+        UserCheck(message="custom rejection message",banned = ['root','admin','sys']),
+        Length(min=2,max=15)
+        ])
+    submit = SubmitField('Sign up')
+
+@app.route('/', methods=['GET','POST'])
+def postName():
+    form = myForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        return render_template('home.html', form = form, username=username)
+    else:
+        return render_template('home.html', form = form, username="")
+
+if __name__ == '__main__':
+    app.run(debug=True)
